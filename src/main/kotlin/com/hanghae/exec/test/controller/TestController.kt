513ba617e2f58de751898ede94126479fe7ff8db
JaeMinHeo95 <rfvgbn21@naver.com>
2023-12-03 14:48:44 +0900

    first commit

---
+++ src/main/kotlin/com/hanghae/exec/test/controller/TestController.kt
@@ -0,0 +1,15 @@
+package com.hanghae.exec.test.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class TextController{
+
+    @GetMapping("/test")
+    fun helloWorld(): String {
+        return "test"
+    }
+}
